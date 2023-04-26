9d3e2f9d4c5ecb43532a0b1d1de2bf63f1233a04
springevidence <exshipbuilder1@gmail.com>
2023-04-26 18:54:38 +0300

    added base layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,44 @@ import './App.css';
 
 const App = () => {
     return (
-        <div>
-            hello
+        <div className="app-wrapper">
+            <header  className="header">
+                <img src="https://seeklogo.com/images/C/company-name-logo-09881CAD1A-seeklogo.com.png" alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://images.all-free-download.com/images/graphiclarge/beach_cloud_dawn_horizon_horizontal_landscape_ocean_601821.jpg" alt="sky"/>
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    New post
+                </div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+            </div>
         </div>
     );
 }
