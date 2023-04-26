1d334603ff4c37e0492b78266e2021f1ea9b1e1e
springevidence <exshipbuilder1@gmail.com>
2023-04-26 10:38:09 +0300

    delete header and technologies components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 const App = () => {
     return (
         <div>
-            <Header/>
-            <Technologies/>
+            hello
         </div>
     );
 }
